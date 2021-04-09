a6e7fb14f104e44cb761c31da21c3058bc426070
nemesio <jsewere19@gmail.com>
2021-04-08 21:34:32 -0400

    vast changes and updates made to personal portfolio

---
+++ fun.js
@@ -0,0 +1,34 @@
+/*
+const { url } = require("node:inspector");
+
+// Global variable to control the scrolling behavior
+const step = 30; // For each 30px, change an image
+
+
+function trackScrollPosition() {
+ const y = window.scrollY;
+ const label = Math.min(Math.floor(y/30) + 1, 20);
+ const imageToUse = fruitImages[label];
+ // Change the backgroung image
+ $('.image-container').css('background-image', `url('${imageToUse}')`);
+
+}
+
+$(document).ready(()=>{
+ $(window).scroll(()=>{
+  trackScrollPosition();
+ })
+})
+
+const textStyle = {
+ 1: {opacity: 0, transform: '0px'},
+ 2: {opacity: 0, transform: '0px'},
+ 3: {opacity: 0, transform: '0px'},
+ 4: {opacity: 0, transform: '0px'},
+ 5: {opacity: .25, transform: '15px'},
+ 6: {opacity: .5, transform: '10px'},
+ 7: {opacity: .75, transform: '5px'},
+ 8: {opacity: 1, transform: '0px'},
+}
+
+*/
