3234744153ea780c20c0c4a1957cc46d25c0c60e
nemesio <jsewere19@gmail.com>
2021-06-10 00:57:12 -0400

    added files and images.

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+             $('.navbar').addClass("sticky");
+        }else{
+             $('.navbar').removeClass("sticky");
+        }
+
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    })
+});
+
